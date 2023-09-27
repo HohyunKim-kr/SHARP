c10df56dc107d02381c4e679613b30ab29f5c17a
Hohyun.Kim <hohyun1022@naver.com>
2023-09-27 13:33:41 +0900

    fix: Board Entity

--- src/board/board.entity.js
+++ src/board/board.entity.js
@@ -9,5 +9,12 @@ class Board {
   hit;
   constructor(userId, category, id, title, writer, content, created_at, hit) {
     this.userId = userId;
+    this.category = category;
+    this.id = id;
+    this.title = title;
+    this.writer = writer;
+    this.content = content;
+    this.created_at = created_at;
+    this.hit = hit;
   }
 }
