7878417f73743142624d90457021831dde32be45
Hohyun.Kim <hohyun1022@naver.com>
2023-10-09 16:42:49 +0900

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,8 @@ const userRouter = require("./user/user.route");
 
 router.get("/", (req, res) => {
   // console.log("!!!!!!");
-  // console.log("indexrouter : ", req.user);
-
-  const isLoggedIn = req.user !== undefined;
-
-  // 로그인한 사용자의 정보를 템플릿에 전달
-  const user = isLoggedIn ? req.user : null;
-
-  // 템플릿을 렌더링
-  res.render("index.html", { user });
-
-  // res.render("index.html", { user: req.user });
+  console.log("indexrouter : ", req.user);
+  res.render("index.html", { user: req.user });
 });
 
 router.use("/boards", boardRouter);
